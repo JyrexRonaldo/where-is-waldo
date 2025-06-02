247ae395d941c04277516bb2254f07f1c3c06fd6
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-02 22:27:57 +0000

    Setup game screen elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import waldoImg from '/waldo.png'
+import odlawImg from '/odlaw.gif'
+import wizardImg from '/wizard.gif'
+import beach from '/beach.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="flex min-h-screen bg-gray-700">
+            <div className="flex w-40 flex-col justify-center gap-8 bg-gray-800 px-4 py-4">
+                <p className="text-white">Timer: </p>
+                <img src={waldoImg} alt="" />
+                <img src={wizardImg} alt="" />
+                <img src={odlawImg} alt="" />
+            </div>
+            <div className="flex max-w-10/12 items-center">
+                <img src={beach} alt="beach picture" />
+            </div>
+        </div>
+    )
 }
 
 export default App
