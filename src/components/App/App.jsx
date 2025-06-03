8621319f71aa58f3868759fa7cc68225d010c79c
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-03 13:10:22 +0000

    Move components into individual files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import waldoImg from '/waldo.png'
 import odlawImg from '/odlaw.gif'
 import wizardImg from '/wizard.gif'
 import beach from '/beach3.webp'
-import './App.css'
 
 function App() {
     const areaClickHandler = (e) => {
@@ -14,27 +13,36 @@ function App() {
         <div className="flex min-h-screen bg-gray-700">
             <div className="flex w-40 flex-col justify-center gap-8 bg-gray-800 px-4 py-4">
                 <p className="text-white">Timer: </p>
-                <img src={waldoImg} alt="" />
-                <img src={wizardImg} alt="" />
-                <img src={odlawImg} alt="" />
+                <button className='flex flex-col items-center'>
+                    <img src={waldoImg} alt="" />
+                    <p className='text-white' >Waldo</p>
+                </button>
+                <button className='flex flex-col items-center'>        
+                    <img src={wizardImg} alt="" />
+                    <p className='text-white' >Wizard</p>
+                </button>
+                <button className='flex flex-col items-center'>
+                    <img src={odlawImg} alt="" />
+                    <p className='text-white' >Odlaw</p>
+                </button>
             </div>
             <div className="flex max-w-10/12 items-center">
                 <map name="beach-map">
-                    <area
+                    <area className='cursor-default'
                         onClick={areaClickHandler}
                         alt="odlaw"
                         coords="273,417,312,504"
                         shape="rect"
                         href="#odlaw"
                     ></area>
-                    <area
+                    <area className='cursor-default'
                         onClick={areaClickHandler}
                         alt="waldo"
                         coords="613,416,656,495"
                         shape="rect"
                         href="#waldo"
                     ></area>
-                    <area
+                    <area className='cursor-default'
                         onClick={areaClickHandler}
                         alt="wizard"
                         coords="727,416,772,491"
@@ -43,7 +51,7 @@ function App() {
                     ></area>
                 </map>
 
-                <img src={beach} useMap="#beach-map" alt="beach-picture"  />
+                <img src={beach} useMap="#beach-map" alt="beach-picture" />
             </div>
         </div>
     )
---
+++ src/components/CharacterDropdown/CharacterDropdown.jsx
@@ -0,0 +1,11 @@
+function CharacterDropdown() {
+    return (
+        <div className="bg-black ">
+            <button>Odlaw</button>
+            <button>Waldo</button>
+            <button>Wizard</button>
+        </div>
+    )
+}
+
+export default CharacterDropdown
